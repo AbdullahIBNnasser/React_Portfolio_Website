70374a6343c63c61f39050e1559f5f390ad5017b
AbdullahIBNnasser <128069919+AbdullahIBNnasser@users.noreply.github.com>
2023-06-13 22:16:40 +0530

    Revert "Git@GitHub.com abdullah ib nnasser/react portfolio website.git"

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -27,19 +27,19 @@ const Contact = () => {
             <MdEmail className='contact__option-icons'/>
             <h4>Email</h4>
             <h5>mohd200207@gmail.com </h5>
-            <a href="mailto:mohd200207@gamil.com" target='_blank' rel='noreferrer'>Send a message</a>
+            <a href="mailto:mohd200207@gamil.com" target='_blank'>Send a message</a>
           </article>
           <article className="contact__option">
             <BsMessenger className='contact__option-icons'/>
             <h4>Messenger</h4>
             <h5>abdullah</h5>
-            <a href="https://m.me/abdullah._.nasser/"target='_blank'rel='noreferrer'>Send a message</a>
+            <a href="https://m.me/abdullah._.nasser/"target='_blank'>Send a message</a>
           </article>
           <article className="contact__option">
             <BsWhatsapp className='contact__option-icons'/>
             <h4>Whatsapp</h4>
             <h5>+123456789</h5>
-            <a href="https://web.whatsapp.com/send?phone=+919080675311"target='_blank'rel='noreferrer'>Send a message</a>
+            <a href="https://web.whatsapp.com/send?phone=+919080675311"target='_blank'>Send a message</a>
           </article>
           </div>
         <form ref={form} onSubmit={sendEmail}>
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,7 +8,6 @@ import {CgScrollH} from 'react-icons/cg'
 function header() {
   return (
     <header>
-      <section>
       <div className="container header__container">
         <h5>Hello I'm</h5>
         <h1>Mohamed Abdullah</h1>
@@ -24,7 +23,6 @@ function header() {
 
 
       </div>
-      </section>
 
     </header>
   )
